a81a4051a0161e91f26a877a07efc028a6dc93bb
Damian Longoni <dlongoni@sancorseguros.com>
2021-04-24 11:55:32 -0300

    FIx#1

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    <div>"Hola Mundo"</div>
-}
+    return <div>Hola Mundo</div>;
+};
 
 ReactDOM.render(<App />, document.querySelector('#root')
 );
