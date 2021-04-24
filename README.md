20536a4c7247fad1b624387d0b6aa4e19de2266a
Damian Longoni <dlongoni@sancorseguros.com>
2021-04-24 11:42:59 -0300

    Hola Mundo

--- README.md
+++
@@ -1,2 +0,0 @@
-# my-app
-My first react app
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    <div>"Hola Mundo"</div>
+}
+
+ReactDOM.render(<App />, document.querySelector('#root')
+);
