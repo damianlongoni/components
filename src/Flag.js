ef79ba4fbd65e068814c864134ae706f5db6f982
Damián Longoni <damianlongoni@gmail.com>
2021-04-25 22:06:40 -0300

    Refactor Flag.js

--- src/Flag.js
+++ src/Flag.js
@@ -1,14 +1,7 @@
 import React from 'react'
 
 const Flag = porps => {
-        
-        switch(porps.nationalityCode) {
-  
-          case "ar":   return <i className="ar flag"></i>;
-          case "al":   return <i className="al flag"></i>;
-          case "br":   return <i className="br flag"></i>;
-          default:      return <i className="ar flag"></i>
-        }
+    return <i className={porps.nationalityCode + ' flag'}></i>;
 };
 
 export default Flag;
